869641cb1a4bef88412bc42b8bbbe63801308a46
Wolferinesda <drhalterman1@yahoo.com>
2018-11-20 13:07:06 -0500

    Trying to retrieve and parse current users

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,5 +1,6 @@
 require 'httparty'
 # require httparty to load when file is loaded
+require 'json'
 
 class Kele
 # Createing Kele class to set basis for kele function
@@ -10,13 +11,18 @@ class Kele
   def initialize(email, password)
 # new initialize method to process the emails and passwords entered
     post_response = self.class.post('/sessions', body: {
-        email: email,
-        password: password
-      })
+      email: email,
+      password: password
+    })
     @user_auth_token = post_response['auth_token']
 # posts the email and password created
     raise "Invalid Email or Password. Try Again." if @user_auth_token.nil?
 # gives error if the token is nil
-   end
+  end
+
+  def get_me
+    response = self.class.get('/users/me', headers: { "authorization" => @user_auth_token })
+    response_parse = JSON.parse(response.body)
+  end
 
 end
